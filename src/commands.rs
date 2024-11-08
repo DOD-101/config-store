6c5fa09098d3c852a6b42f505daa20c6284565df
David Thievon <pkfan@gmail.com>
2024-11-08 10:45:21 +0100

    refactor(commands): move commands / logic to commands module

---
+++ src/commands.rs
@@ -0,0 +1,121 @@
+use rusqlite::{Connection, Result};
+use std::{fmt::Write, process::exit};
+
+#[derive(Debug)]
+struct Value {
+    id: i32,
+    name: String,
+    value: String,
+    alternate: String,
+}
+
+fn select(connection: &Connection, name: &str) -> Result<Value, rusqlite::Error> {
+    connection.query_row("SELECT * FROM data WHERE name = ?", [name], |row| {
+        Ok(Value {
+            id: row.get(0)?,
+            name: row.get(1)?,
+            value: row.get(2)?,
+            alternate: row.get(3)?,
+        })
+    })
+}
+
+fn exists(connection: &Connection, name: &str) -> Result<bool, rusqlite::Error> {
+    connection
+        .prepare("SELECT name FROM data WHERE name = ?")?
+        .exists([name])
+}
+
+fn new(
+    connection: &Connection,
+    name: String,
+    value: String,
+    alternate: String,
+) -> Result<String, rusqlite::Error> {
+    connection.execute(
+        "INSERT INTO data (name, value, alternate) VALUES (?1, ?2, ?3)",
+        [name, value, alternate],
+    )?;
+
+    Ok(String::from("Ok"))
+}
+
+pub fn exists_cmd(connection: &Connection, name: String) -> Result<String, rusqlite::Error> {
+    match exists(connection, &name) {
+        Ok(b) => Ok(b.to_string()),
+        Err(e) => Err(e),
+    }
+}
+
+pub fn delete_cmd(connection: &Connection, name: String) -> Result<String, rusqlite::Error> {
+    connection.execute("DELETE FROM data WHERE name = ?", [name])?;
+
+    Ok("Ok".to_string())
+}
+
+pub fn get_cmd(connection: &Connection, name: String) -> Result<String, rusqlite::Error> {
+    let value = select(connection, &name)?;
+
+    Ok(value.name)
+}
+
+pub fn set_cmd(
+    connection: &Connection,
+    name: String,
+    new_value: Option<String>,
+    new_alternate: Option<String>,
+    change_only: bool,
+) -> Result<String, rusqlite::Error> {
+    if exists(connection, &name)? {
+        let value = select(connection, &name)?;
+
+        connection.execute(
+            "UPDATE data SET value = ?, alternate = ? WHERE name = ?",
+            [
+                new_value.unwrap_or(value.value.clone()),
+                new_alternate.unwrap_or(value.alternate.clone()),
+                name,
+            ],
+        )?;
+
+        Ok("Ok".to_string())
+    } else if !change_only {
+        new(
+            connection,
+            name,
+            new_value.unwrap_or_default(),
+            new_alternate.unwrap_or_default(),
+        )
+    } else {
+        eprintln!("Trying to change a value that doesn't exist.");
+        exit(1)
+    }
+}
+
+pub fn toggle_cmd(connection: &Connection, name: String) -> Result<String, rusqlite::Error> {
+    let value = select(connection, &name)?;
+
+    connection.execute(
+        "UPDATE data SET value = ?, alternate = ? WHERE name = ?",
+        [value.alternate, value.value.clone(), value.name],
+    )?;
+
+    Ok(value.value)
+}
+
+pub fn list_cmd(connection: &Connection) -> Result<String, rusqlite::Error> {
+    Ok(connection
+        .prepare("SELECT * FROM data")?
+        .query_map([], |row| {
+            Ok(Value {
+                id: row.get(0)?,
+                name: row.get(1)?,
+                value: row.get(2)?,
+                alternate: row.get(3)?,
+            })
+        })?
+        .fold(String::new(), |mut acc, e| {
+            writeln!(acc, "{:?}", e.unwrap()).unwrap();
+            acc
+        }))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,9 @@
 use core::panic;
-use std::fmt::Write;
-use std::process::exit;
 
 use clap::{Parser, Subcommand};
 use rusqlite::{Connection, Result};
 
-#[derive(Debug)]
-struct Value {
-    id: i32,
-    name: String,
-    value: String,
-    alternate: String,
-}
+mod commands;
 
 fn main() -> Result<()> {
     let args = Args::parse();
@@ -41,16 +33,16 @@ fn main() -> Result<()> {
             value,
             alternate,
             change_only,
-        } => set_cmd(&connection, name, value, alternate, change_only),
+        } => commands::set_cmd(&connection, name, value, alternate, change_only),
         Action::Get {
             name,
             value_only,
             alternate_only,
-        } => get_cmd(&connection, name),
-        Action::Toggle { name } => toggle_cmd(&connection, name),
-        Action::Delete { name } => delete_cmd(&connection, name),
-        Action::Check { name } => exists_cmd(&connection, name),
-        Action::List => list_cmd(&connection),
+        } => commands::get_cmd(&connection, name),
+        Action::Toggle { name } => commands::toggle_cmd(&connection, name),
+        Action::Delete { name } => commands::delete_cmd(&connection, name),
+        Action::Check { name } => commands::exists_cmd(&connection, name),
+        Action::List => commands::list_cmd(&connection),
     }?;
 
     println!("{}", result);
@@ -106,115 +98,4 @@ enum Action {
     Check { name: String },
     /// List the contents of the db
     List,
-}
-
-fn select(connection: &Connection, name: &str) -> Result<Value, rusqlite::Error> {
-    connection.query_row("SELECT * FROM data WHERE name = ?", [name], |row| {
-        Ok(Value {
-            id: row.get(0)?,
-            name: row.get(1)?,
-            value: row.get(2)?,
-            alternate: row.get(3)?,
-        })
-    })
-}
-
-fn exists(connection: &Connection, name: &str) -> Result<bool, rusqlite::Error> {
-    connection
-        .prepare("SELECT name FROM data WHERE name = ?")?
-        .exists([name])
-}
-
-fn new(
-    connection: &Connection,
-    name: String,
-    value: String,
-    alternate: String,
-) -> Result<String, rusqlite::Error> {
-    connection.execute(
-        "INSERT INTO data (name, value, alternate) VALUES (?1, ?2, ?3)",
-        [name, value, alternate],
-    )?;
-
-    Ok(String::from("Ok"))
-}
-
-fn exists_cmd(connection: &Connection, name: String) -> Result<String, rusqlite::Error> {
-    match exists(connection, &name) {
-        Ok(b) => Ok(b.to_string()),
-        Err(e) => Err(e),
-    }
-}
-
-fn delete_cmd(connection: &Connection, name: String) -> Result<String, rusqlite::Error> {
-    connection.execute("DELETE FROM data WHERE name = ?", [name])?;
-
-    Ok("Ok".to_string())
-}
-
-fn get_cmd(connection: &Connection, name: String) -> Result<String, rusqlite::Error> {
-    let value = select(connection, &name)?;
-
-    Ok(value.name)
-}
-
-fn set_cmd(
-    connection: &Connection,
-    name: String,
-    new_value: Option<String>,
-    new_alternate: Option<String>,
-    change_only: bool,
-) -> Result<String, rusqlite::Error> {
-    if exists(connection, &name)? {
-        let value = select(connection, &name)?;
-
-        connection.execute(
-            "UPDATE data SET value = ?, alternate = ? WHERE name = ?",
-            [
-                new_value.unwrap_or(value.value.clone()),
-                new_alternate.unwrap_or(value.alternate.clone()),
-                name,
-            ],
-        )?;
-
-        Ok("Ok".to_string())
-    } else if !change_only {
-        new(
-            connection,
-            name,
-            new_value.unwrap_or_default(),
-            new_alternate.unwrap_or_default(),
-        )
-    } else {
-        eprintln!("Trying to change a value that doesn't exist.");
-        exit(1)
-    }
-}
-
-fn toggle_cmd(connection: &Connection, name: String) -> Result<String, rusqlite::Error> {
-    let value = select(connection, &name)?;
-
-    connection.execute(
-        "UPDATE data SET value = ?, alternate = ? WHERE name = ?",
-        [value.alternate, value.value.clone(), value.name],
-    )?;
-
-    Ok(value.value)
-}
-
-fn list_cmd(connection: &Connection) -> Result<String, rusqlite::Error> {
-    Ok(connection
-        .prepare("SELECT * FROM data")?
-        .query_map([], |row| {
-            Ok(Value {
-                id: row.get(0)?,
-                name: row.get(1)?,
-                value: row.get(2)?,
-                alternate: row.get(3)?,
-            })
-        })?
-        .fold(String::new(), |mut acc, e| {
-            writeln!(acc, "{:?}", e.unwrap()).unwrap();
-            acc
-        }))
 }
